7f082751d7b793c947174341bc3a9da42bf1a644
Renat Bikmukhamedov <50679603+MAN5ON@users.noreply.github.com>
2023-01-30 23:51:36 +0300

    feat(global): done with css and add onClick

--- src/components/main/Buttons.jsx
+++ src/components/main/Buttons.jsx
@@ -3,7 +3,12 @@ import s from "../../styles/Button.module.css";
 
 export const Buttons = function ({ imgSrc, name }) {
   return (
-    <div className={s.button}>
+    <div
+      className={s.button}
+      onClick={function () {
+        alert(`Clicked on "${name}" button`);
+      }}
+    >
       <img src={imgSrc} className={s.btnImgSrc} />
       <div className={s.btnName}>{name}</div>
     </div>
